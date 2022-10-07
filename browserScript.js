0fc00f9502cf736a90037531ea58db1c02eff930
enderonSchool <97123819+enderonSchool@users.noreply.github.com>
2022-10-07 13:41:11 -0400

    Delete browserScript.js

--- browserScript.js
+++
@@ -1,6 +0,0 @@
-function getID() {
-    let id = document.getElementById("projectID").value;
-    let link = "https://scratch.mit.edu/projects/" + id.toString() + "/embed";
-    document.getElementById("iframeContainer").innerHTML = '<iframe id="projectContainer" width="1200" height="750" frameborder="0" scrolling="no" allowfullscreen></iframe>'
-    document.getElementById("projectContainer").src = link;
-}
